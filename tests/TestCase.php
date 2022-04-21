9af37928647d27b36dda4b912deb8be99a780d27
Darren Edale <darren@equituk.net>
2022-04-21 18:36:00 +0100

    Updated TestCase base class
    - added helper to generate anonymous Stringable objects with specified strings

--- tests/TestCase.php
+++ tests/TestCase.php
@@ -4,10 +4,39 @@ declare(strict_types=1);
 
 namespace Equit\Totp\Tests;
 
+use Equit\Totp\Tests\Constraints\EquivalentOtpAuthUrl;
 use PHPUnit\Framework\TestCase as FrameworkTestCase;
+use Stringable;
 
 class TestCase extends FrameworkTestCase
 {
+    /**
+     * Helper to create a Stringable instance for testing purposes.
+     *
+     * The stringable will always return the provided string from its __toString() method.
+     *
+     * @param string $str
+     *
+     * @return \Stringable
+     */
+    protected static function createStringable(string $str): Stringable
+    {
+        return new class($str) implements Stringable
+        {
+            public string $str;
+
+            public function __construct(string $str)
+            {
+                $this->str = $str;
+            }
+
+            public function __toString(): string
+            {
+                return $this->str;
+            }
+        };
+    }
+
 	/**
 	 * Assert that a string contains only characters that are present in another string.
 	 *
@@ -15,8 +44,22 @@ class TestCase extends FrameworkTestCase
 	 * @param string $actualString The string to test.
 	 * @param string $message Optional message for use when the assertion fails.
 	 */
-	public static function assertStringContainsOnly(string $allowableCharacters, string $actualString, string $message = "")
+	public static function assertStringContainsOnly(string $allowableCharacters, string $actualString, string $message = ""): void
 	{
 		static::assertThat($actualString, new Constraints\StringContainsOnly($allowableCharacters), $message);
 	}
+
+    /**
+     * Assert that an OTP provisioning URL is equivalent to another.
+     *
+     * @param string $referenceUrl The URL that the tested URL should match.
+     * @param string $actualUrl The URL to test.
+     * @param string $message Optional message for use when the assertion fails.
+     *
+     * @throws \Equit\Totp\Tests\Exceptions\InvalidOtpUrlException if the reference URL is found not to be valid.
+     */
+    public static function assertOtpUrlIsEquivalentTo(string $referenceUrl, string $actualUrl, string $message = ""): void
+    {
+        static::assertThat($actualUrl, new EquivalentOtpAuthUrl($referenceUrl), $message);
+    }
 }
